e77e4668a45cc058e24f633ff239ed0c2874c97d
Kevin Vega <kevinvcm@gmail.com>
2012-03-15 22:20:19 -0500

    datos de prueba - 5

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+devs = User.create([
+{name: "Kevin", last_name:"Vega Cuevas", email:"kevin@aol.com", birthday:"15/10/1983"},
+{name: "Fiorella", last_name:"Puch S.", email:"fiore@aol.com", birthday:"02/04/1986"},
+])
+
+admin = User.create([
+{name: "Kevin", last_name:"Vega Cuevas", email:"kevin@aol.com", birthday:"15/10/1983"}
+])
+
+Rol.create(description:'Administrator',users:admin)
+Rol.create(description:'Developers',users:devs)
