599bc1cf39c905e1ead557487eb7c0e255e56397
Kevin Vega <kevinvcm@gmail.com>
2012-03-15 20:52:02 -0500

    push inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TdewPc1::Application.config.secret_token = '53f3cb2a54077599a61dbdb7e8ace6766942bfa8e246ac0f2866c3fe9e9c4a9b730ac119e54b431ca13a1104a454af44544b6024f0c8cee474fa1d5c1e8b3110'
